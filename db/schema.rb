67bd82250ba426d2bad049e97c6f5eb2f2fbac4e
加藤美香 <mikarin0901@gmail.com>
2026-06-02 20:07:45 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_01_110731) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_02_104841) do
+  create_table "messages", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "room_users", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
